2f8ee8b37603c4c1669044cbeda70a3f6aca9b42
koux2 <koux2.g@gmail.com>
2012-10-12 15:14:07 +0900

    itinial commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Hideki Eguchi on 2012/10/12.
+//  Copyright (c) 2012年 Hideki Eguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
